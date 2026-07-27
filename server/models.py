b34f76510477ff86e22629c40a609f5e906d1b57
Eugene Kimathi <eugenekimathi@gmail.com>
2026-07-27 20:53:24 +0300

    Implement table constraints and model validations

--- server/models.py
+++ server/models.py
@@ -8,12 +8,38 @@ class Exercise(db.Model):
     __tablename__ = 'exercises'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    category = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(100), nullable=False , unique=True)
+    category = db.Column(db.String(50), nullable=False)
     equipment_needed = db.Column(db.Boolean, default=False)
 
     workout_exercises = db.relationship("WorkoutExercise", back_populates="exercise", cascade="all, delete-orphan")
 
+    # Model Validation
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Exercise name cannot be empty.")
+        if len(value.strip()) < 2:
+            raise ValueError("Exercise name must be at least 2 characters.")
+
+        return value.strip()
+
+    @validates("category")
+    def validate_category(self, key, value):
+
+        allowed_categories = [
+            "Strength",
+            "Cardio",
+            "Flexibility",
+            "Balance"
+        ]
+
+        if value not in allowed_categories:
+            raise ValueError(
+                f"Category must be one of {allowed_categories}"
+            )
+
+        return value
 
 class Workout(db.Model):
     __tablename__ = 'workouts'
@@ -24,6 +50,18 @@ class Workout(db.Model):
     notes =db.Column(db.Text)
 
     workout_exercises = db.relationship("WorkoutExercise", back_populates="workout", cascade="all, delete-orphan")
+
+    # Table Constraints
+    __table_args__ = (
+        db.CheckConstraint('duration > 0', name='check_duration_positive'),
+    )
+
+    # Model Validation
+    @validates("duration")
+    def validate_duration(self, key, value):
+        if value <= 0:
+            raise ValueError("Duration must be a positive integer.")
+        return value
 
 
 class workoutExercise(db.Model):
@@ -39,8 +77,18 @@ class workoutExercise(db.Model):
     workout = db.relationship("Workout", back_populates="workout_exercises")
     exercise = db.relationship("Exercise", back_populates="workout_exercises")
 
+    # Model Validation
+    @validates("reps", "sets", "duration_seconds")
+    def validate_workout_exercise_fields(self, key, value):
+        if value is not None and value < 0:
+            raise ValueError(f"{key} must be a non-negative integer.")
+        return value
 
-
+    # Table Constraints
+    __table_args__ = (
+        db.CheckConstraint('reps >= 0', name='check_reps_non_negative'),
+        db.CheckConstraint('sets >= 0', name='check_sets_non_negative'),
+        db.CheckConstraint('duration_seconds >= 0', name='check_duration_seconds_non_negative'),)
 
 
    
