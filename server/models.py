93e16e776a166cbb7813ca86c1ee4ba12a1c52be
Eugene Kimathi <eugenekimathi@gmail.com>
2026-07-27 19:41:12 +0300

    Create Exercise model

--- server/models.py
+++ server/models.py
@@ -4,6 +4,16 @@ db = SQLAlchemy()
 
 # Define Models here
 
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, default=False)
+
+
+
 
 
 
